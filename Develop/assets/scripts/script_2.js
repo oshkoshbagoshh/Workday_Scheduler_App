731089b1f571aebe228c34a73b61226bfacf3596
AJ Javadi <amirjavadi25@gmail.com>
2023-05-28 04:38:19 -0500

    updates to v2

--- Develop/assets/scripts/script_2.js
+++ Develop/assets/scripts/script_2.js
@@ -3,7 +3,7 @@
  * @Email: someone@example.com
  * @Date: 2023-05-26 05:13:09 
  * @Last Modified by: Someone
- * @Last Modified time: 2023-05-26 06:20:19
+ * @Last Modified time: 2023-05-28 04:37:35
  * @Description: file:///Users/aj/Desktop/bootcamp/Homework/5.%20Workday%20Scheduler/Workday_Scheduler_App/Develop/assets/scripts/script_2.js
  
 
@@ -43,20 +43,69 @@
  * 
 **********/
 console.log(dayjs().format("MMM D, YYYY"));
-currentDate = dayjs().format("MMM D, YYYY");
-currentTime = dayjs().format("DD/MM/YYYY")
+var currentDate = dayjs().format("MMM D, YYYY");
+var currentTime = dayjs().format("DD/MM/YYYY")
 console.log('currentTime = dayjs().format("DD/MM/YYYY"): ', currentTime = dayjs().format("DD/MM/YYYY"));
 
+var listOfblocks;
+
+var elements = document.querySelectorAll('.time-button');
+
+
+//currentHour
+var currentHour = dayjs().format("h");
+window.alert("the current hour is" + " " + currentHour);
+console.log(currentHour);
 // $(selectors);
 // window.alert("Hello, world!");
 typeof(currentDate)
 console.log('typeof(currentDate): ', typeof(currentDate));
 
 
-$(".saveButton").click(function (e) {
-    // Do anything
+
+function checkCurrentHour () {
+    // if the time blocks are equal to the current hour variable up above then change the class
+
+    var isCurrentHourCheck = false;
+    // if else loop for all of the blocks in the html
+    
+    var divElement = document.getElementsByClassName ("div");
+ 
+
+
+
+
+//END
+
+    return checkCurrentHour();
 
     
-    $()
-});
+}
 
+
+
+
+
+//create save button
+$(".saveBtn").click(function (e) {
+    // Do anything
+    window.alert("event1");
+localStorage.setItem("cat","dog");
+
+
+
+
+window.addEventListener("storage", (e) => {
+    document.querySelector("textarea").textContent = e.textContent;
+    // document.querySelector("").textContent = e.oldValue;
+    // document.querySelector(".my-new").textContent = e.newValue;
+    // document.querySelector(".my-url").textContent = e.url;
+    // document.querySelector(".my-storage").textContent = JSON.stringify(
+      e.storageArea
+
+}
+    );
+
+  });
+
+    
--- README.md
+++ README.md
@@ -52,6 +52,9 @@ WHEN I refresh the page
 THEN the saved events persist
 ```
 
+#TODO: // edit from here
+
+<!----
 ## Table of Contents (Optional)
 
 If your README is long, add a table of contents to make it easy for users to find what they need.
